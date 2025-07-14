1a4b715481d60ae59eee601c90cfa575eca2019a
Dika yonanda putra <yonandaputra05@gmail.com>
2025-07-14 23:36:23 +0700

    Add navbar

--- resources/views/pages/shared/landing/index.blade.php
+++ resources/views/pages/shared/landing/index.blade.php
@@ -1,8 +1,3 @@
-<x-app-layout title="Home">
-    <section class=" flex items-center justify-center min-h-screen">
-        <div class=" bg-bg-main rounded-md shadow p-5">
-            <h1 class=" text-5xl font-bold">Login aja bang ini urusan dika</h1>
-            <a href="{{ route('login') }}" class="text-2xl mt-2 block">Login aja bang</a>
-        </div>
-    </section>
-</x-app-layout>
+<x-layouts.app title="Home">
+    <x-partials.user-navbar></x-partials.user-navbar>
+</x-layouts.app>
