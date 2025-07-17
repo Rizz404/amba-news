8be208f05e8af552da88528216f6f7fb6f97b232
Dika yonanda putra <yonandaputra05@gmail.com>
2025-07-17 15:49:50 +0700

    rapihin kode

--- resources/views/components/partials/user-navbar.blade.php
+++ resources/views/components/partials/user-navbar.blade.php
@@ -1,37 +1,39 @@
 <!-- NAVBAR -->
-    <nav class="bg-white p-4">
-      <div class="container mx-auto items-center justify-between font-semibold">
+<nav class="bg-white p-4">
+    <div class="container mx-auto items-center justify-between font-semibold">
         <div class="mx-4 flex justify-between space-y-2">
-          <div class="flex space-x-4">
-            <!-- Logo -->
-            <div>logo</div>
+            <div class="flex space-x-4">
+                <!-- Logo -->
+                <div>logo</div>
 
-            <!-- Searchbar -->
-            <div class="flex items-center space-x-2">
-              <input type="text" placeholder="Search news..." class="rounded-full border border-gray-300 w-68 px-4 py-1.5 text-sm focus:outline-orange-300"/>
-              <button class="cursor-pointer rounded-full bg-orange-500 px-4 py-1.5 text-sm font-semibold text-white transition">Search</button>
+                <!-- Searchbar -->
+                <div class="flex items-center space-x-2">
+                    <input type="text" placeholder="Search news..."
+                        class="rounded-full border border-gray-300 w-68 px-4 py-1.5 text-sm focus:outline-orange-300" />
+                    <button
+                        class="cursor-pointer rounded-full bg-orange-500 px-4 py-1.5 text-sm font-semibold text-white transition">Search</button>
+                </div>
             </div>
-          </div>
 
-          <!-- Auth Button -->
-          <div class="space-x-2">
-            <a href="{{ route('login') }}" class="hover:text-orange-500">Login</a>
-            <a href="#" class="hover:text-orange-500">Register</a>
-          </div>
+            <!-- Auth Button -->
+            <div class="space-x-2">
+                <a href="{{ route('login') }}" class="hover:text-orange-500">Login</a>
+                <a href="#" class="hover:text-orange-500">Register</a>
+            </div>
         </div>
 
         <!-- Category -->
         <ul class="flex space-x-6 text-black">
-          <li><a href="#" class="hover:text-orange-500">Home</a></li>
-          <li><a href="#" class="hover:text-orange-500">News</a></li>
-          <li><a href="#" class="hover:text-orange-500">Sus</a></li>
-          <li><a href="#" class="hover:text-orange-500">Football</a></li>
-          <li><a href="#" class="hover:text-orange-500">TV</a></li>
-          <li><a href="#" class="hover:text-orange-500">Technology</a></li>
+            <li><a href="#" class="hover:text-orange-500">Home</a></li>
+            <li><a href="#" class="hover:text-orange-500">News</a></li>
+            <li><a href="#" class="hover:text-orange-500">Sus</a></li>
+            <li><a href="#" class="hover:text-orange-500">Football</a></li>
+            <li><a href="#" class="hover:text-orange-500">TV</a></li>
+            <li><a href="#" class="hover:text-orange-500">Technology</a></li>
         </ul>
-      </div>
+    </div>
 
-      {{-- Horizontal Line --}}
-      <hr class="border-gray-300">
-    </nav>
+    {{-- Horizontal Line --}}
+    <hr class="border-gray-300">
+</nav>
 <!-- END NAVBAR -->
